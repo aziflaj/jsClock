f324c54c2d820ca2a0340d78c8e130049dfd9d08
aziflaj <aldoziflaj95@gmail.com>
2015-02-09 21:33:58 -0800

    fixed small bug

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 var clock = function() {
     var date = new Date();
 
-    var hours = (date.getHours() > 10 ? date.getHours() : '0' + date.getHours());
-    var mins = (date.getMinutes() > 10 ? date.getMinutes() : '0' + date.getMinutes());
-    var secs = (date.getSeconds() > 10 ? date.getSeconds() : '0' + date.getSeconds());
+    var hours = (date.getHours() >= 10 ? date.getHours() : '0' + date.getHours());
+    var mins = (date.getMinutes() >= 10 ? date.getMinutes() : '0' + date.getMinutes());
+    var secs = (date.getSeconds() >= 10 ? date.getSeconds() : '0' + date.getSeconds());
 
     var hexColor = "#" + hours + mins + secs;
 
